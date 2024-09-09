90af7726963ef9c2845ab65ae08d14453ed45a29
Mohamed Abdelmenam <mabdelmenam@gmail.com>
2024-09-08 22:47:22 -0400

    Added a keyword and identifier check

--- lexer.py
+++ lexer.py
@@ -1,4 +1,5 @@
 from enum import Enum
+import sys
 class Lexer: 
     def __init__(self, source):
         self.source = source + '\n'
@@ -13,7 +14,7 @@ class Lexer:
             self.currChar = '\0' #sets the current character equal to EOF character
         else:
             self.currChar = self.source[self.currPos] #if not at EOF then set the curent character equal to the character at the currPos in the source string
-        pass
+        return self.currChar
     #Looks at the next character but doesnt move currPos , used when i want to check what comes next
     def peek(self):
         if self.currPos + 1 >= len(self.source):
@@ -21,16 +22,25 @@ class Lexer:
         else:
             return self.source[self.currPos + 1] #checks the next character
     #Handles errors, if it sees an invalid token, program exits
-    def abort(self):
+    def abort(self, message):
+        sys.exit("Exiting....\n" + message)
         pass
     #Skips all the spaces and tabs until it finds a new line \n
     def skipWhiteSpace(self):
-        pass
+        while self.currChar == ' ' or self.currChar == '\t' or self.currChar == '\r':
+            self.nextChar()
     #If lexer detects a comment, starting with " // " it will skip over the whole comment until it finds a newline
     def skipComment(self):
-        pass
+        if self.currChar == '@':
+            if self.peek() == '@':
+                while self.currChar != '\n':
+                    self.nextChar()
+            else:
+                self.abort("Unknown Token: " + self.peek())
     #Core function which will return the next token in the source code. Will be used to classify the current sequence of chars into different token types
     def getToken(self):
+        self.skipWhiteSpace()
+        self.skipComment()
         token = None
         if self.currChar == '+': #PLUS
             token = Token(self.currChar, TokenType.PLUS)
@@ -40,7 +50,7 @@ class Lexer:
             if self.peek() == '=':
                 previousChar = self.currChar
                 nextChar = self.nextChar()
-                token = Token(previousChar + nextChar, TokenType.EqEq) #DOUBLE EQUALS
+                token = Token(previousChar + nextChar, TokenType.EqEq) #DOUBLE EQUALS 
             else:
                 token = Token(self.currChar, TokenType.EQUALS)
         elif self.currChar == '>':#Greater than and Greater than or Equals
@@ -56,15 +66,77 @@ class Lexer:
                 nextChar = self.nextChar()
                 token = Token(previousChar + nextChar, TokenType.LESSTHANEQ)
             else:
-                token = Token(self.currChar)
+                token = Token(self.currChar, TokenType.LESSTHAN)
+        elif self.currChar == '!': #Not Equals
+            if self.peek() == '=':
+                previousChar = self.currChar
+                nextChar  = self.nextChar()
+                token = Token(previousChar + nextChar, TokenType.NOTEQUALS)
+            else: 
+                self.abort("Expected !=, but instead receive ! + " + self.peek())
         elif self.currChar == '*':#Asterisk
             token = Token(self.currChar, TokenType.ASTERISK)
         elif self.currChar == "/":#Slash
             token = Token(self.currChar, TokenType.SLASH)
+        elif self.currChar == '\n': 
+            token = Token(self.currChar, TokenType.NEWLINE)
         elif self.currChar == '\0': #End of File
             token = Token(self.currChar, TokenType.EOF)
+        #Printing a String
+        elif self.currChar == '\"':
+            #start reading the string
+            self.nextChar() #in the string
+            startPosition = self.currPos
+            while self.currChar != "\"":
+                if self.currChar == '\r' or self.currChar == '\n' or self.currChar == '\t' or self.currChar == '\\' or self.currChar == '%':
+                    self.abort("Illegal character in string.")
+                self.nextChar()
+            tokenText = ''
+            for i in range(startPosition, self.currPos):
+                tokenText += self.source[i]
+            print("Token text: ", tokenText)
+            token = Token(tokenText, TokenType.STRING)
+        #Dealing with digits
+        elif self.currChar.isdigit():
+            startPosition = self.currPos
+            while self.peek().isdigit():
+                self.nextChar()
+
+            if self.peek() == '.': #decimal
+                self.nextChar()
+                if not self.peek().isdigit():
+                    self.abort("Chracter is not a number")
+                while self.peek().isdigit():
+                    self.nextChar()
+
+            tokenText = ''
+            for i in range(startPosition, self.currPos + 1):
+                tokenText += self.source[i]
+            token = Token(tokenText, TokenType.NUMBER)
+        #Check whether text is an identifier or keyword ------------------------------------------------
+        elif self.currChar.isalpha():
+            #start looping here
+            startPosition = self.currPos
+            while( self.peek().isalpha() ):
+                self.nextChar()
+                #print("in: ",self.currChar)
+            
+            tokenText = ""
+            for i in range( startPosition, self.currPos + 1): 
+                tokenText += self.source[i]
+                #print("Self source: " , "\n", self.source[i], "\n")
+            
+            is_a_keyword = Token.keywordCheck(tokenText)
+            #print("is a keyword: ", is_a_keyword)
+            if is_a_keyword == None:
+                token = Token(tokenText, TokenType.IDENTIFIER)
+            else:
+                token = Token(tokenText, is_a_keyword) #returned the keyword TokenType.x
+            #check whether joey is a keyword or not
+
+
         else:
-            pass
+            self.abort("Unknown Token: " + self.currChar)
         self.nextChar()
         return token
 
@@ -72,16 +144,31 @@ class Token:
     def __init__(self, tokenText, tokenType):
         self.text = tokenText
         self.Type = tokenType
+    def keywordCheck(text):
+        for x in TokenType:
+            if x.name == text and x.value >= 50 and x.value <=59:
+                #is a keyword
+                return x
+        return None
 
 class TokenType(Enum):
+    #name = value
     EOF = -1
     NEWLINE = 0
     NUMBER = 1
     IDENTIFIER = 2
     STRING = 3
     #Keywords
-    label = 51
-    goto = 52 
+    LABEL = 50
+    GOTO = 51 
+    PRINT = 52
+    VAR = 53
+    IF = 54
+    THEREFORE = 55
+    ENDIF = 56
+    WHILE = 57
+    DONTSTOP = 58
+    ENDWHILE = 59
     #Operators
     EQUALS = 100
     PLUS = 101
